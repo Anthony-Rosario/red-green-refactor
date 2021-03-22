83aa18066ad8ef79425151b9718cc1b93144cd25
Anthony Rosario <anthonymrosario225@gmail.com>
2021-03-22 13:21:46 -0700

    test passing and hopefully destructured correctly

--- 1-getName/getName.js
+++ 1-getName/getName.js
@@ -1,14 +1,3 @@
-const jeep = {
-  name: 'jeep',
-  age: 2,
-  weight: '10 lbs'
-};
-
-const character = {
-  _id: '5cf5679a915ecad153ab68c9',
-  name: 'Aang'
-};
-
 const getName = (obj) => {
   const { name } = obj;
   return name;
--- 1-getName/getName.test.js
+++ 1-getName/getName.test.js
@@ -1,13 +1,24 @@
 const { getName } = require('./getName.js');
-
-test('returns name of an object', () => {
-  const jeep = {
-    name: 'jeep',
-    age: 2,
-    weight: '10 lbs'
-  };
+describe('function getName returns the name property from an object', () => {
+  it('returns name of an object', () => {
+    const jeep = {
+      name: 'jeep',
+      age: 2,
+      weight: '10 lbs'
+    };
   
-  const name = getName(jeep);
+    const name = getName(jeep);
 
-  expect(name).toEqual('jeep');
+    expect(name).toEqual('jeep');
+  });
+
+  it('returns the name property of any object', () => {
+    const character = {
+      _id: '5cf5679a915ecad153ab68c9',
+      name: 'Aang'
+    };
+    const name = getName(character);
+
+    expect(name).toEqual('Aang');
+  });
 });
