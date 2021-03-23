51916088f53dcd2792e3bafa517c3fe2897c6ece
Anthony Rosario <anthonymrosario225@gmail.com>
2021-03-22 17:34:50 -0700

    function works correctly with passing test and for loop

--- 3-capitalizeAndFilter/capitalizeAndFilter.js
+++ 3-capitalizeAndFilter/capitalizeAndFilter.js
@@ -1,5 +1,15 @@
-const capitalizeAndFilter = () => {
+const capitalizeAndFilter = (arr) => {
+  const newArr = [];
 
+  for(let i = 0; i < arr.length; i++) {
+    const diffArray = arr[i];
+    const upperCaseString = diffArray.toUpperCase();
+
+    if(upperCaseString.charAt(0) !== 'F') {
+      newArr.push(upperCaseString);
+    }
+  }
+  return newArr;
 };
 
 module.exports = { capitalizeAndFilter };
--- 3-capitalizeAndFilter/capitalizeAndFilter.test.js
+++ 3-capitalizeAndFilter/capitalizeAndFilter.test.js
@@ -1,5 +1,11 @@
-describe('', () => {
-  it('', () => {
+const { capitalizeAndFilter } = require('./capitalizeAndFilter.js');
 
+describe('capitalizeAndFilter takes an array of strings', () => {
+  it('capitalizes all strings, and filters out any string that starts with the letter f', () => {
+    const names = ['Derrick', 'Chad', 'Fred', 'kyle', 'Franklin'];
+    
+    const result = capitalizeAndFilter(names);
+
+    expect(result).toEqual(['DERRICK', 'CHAD', 'KYLE']);
   });
 });
